dd74e3dd33034cfb1b22635f066e92db43cc9420
Pranesh <pvallabh@jhb.dvt.co.za>
2018-09-10 08:56:45 +0200

    Clear the generateMocks file

--- SwiftCollectionView/SwiftCollectionView/AppDelegate.swift
+++ SwiftCollectionView/SwiftCollectionView/AppDelegate.swift
@@ -17,15 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let dependencyRegisration =  DependencyRegistration()
         dependencyRegisration.registerDependency()
-       // dependencyRegistration.registerDependency()
-        /*let container = DepedencyContainer.instance
-        container.register(dependecy: BeerDataGetable.self, implementation: {
-            BeerRepository()
-        })
-        container.register(dependecy: Networkable.self, implementation: {
-            Networking()
-        })*/
-        
         return true
     }
 
